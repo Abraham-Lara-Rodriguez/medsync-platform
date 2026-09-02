853896d6a1c771276510d101a5912d7416ad49f5
Abraham Lara Rodriguez <abrahamopelara@hotmail.com>
2026-09-01 20:42:58 -0500

    style(common-security): add blank line in ResourceServerJwtFilter
    Minor formatting adjustment (single blank line). No logic change.

--- common-security/src/main/java/com/medsync/commonsecurity/jwt/ResourceServerJwtFilter.java
+++ common-security/src/main/java/com/medsync/commonsecurity/jwt/ResourceServerJwtFilter.java
@@ -18,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 
+
 /**
  * Stateless resource-server filter for every service EXCEPT auth-service.
  * <p>
